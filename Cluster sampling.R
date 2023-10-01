3af051eafbaf78353d2c2612f40c33d96b8f9727
G3ordan1 <geordan.ramsamy@gmail.com>
2023-10-01 21:35:26 +0400

    edited cluster sampling.R

--- Cluster sampling.R
+++ Cluster sampling.R
@@ -14,37 +14,31 @@ N <- 96
 
 # (i) Estimate the average repair cost per saw for the past month and its variance:
 
-# Calculate the average repair cost per saw for each industry
-data <- data %>% mutate(Average_Repair_Cost_Per_Saw = Total_Repair_Cost / Number_of_Saws)
+#s2r <- sum((data$Total_Repair_Cost - ybar * data$Number_of_Saws)^2) / (n - 1)
+#var_ybar <- s2r / (sum(data$Number_of_Saws)^2 / n) * (1 - n / N)
 
-# Calculate the sample mean and sample variance of repair costs per saw
-sample_mean <- mean(data$Average_Repair_Cost_Per_Saw)
-sample_variance <- var(data$Average_Repair_Cost_Per_Saw)
+sum_yi <- sum(data$Total_Repair_Cost)
+sum_mi <- sum(data$Number_of_Saws)
+sum_mi2 <- sum(data$Number_of_Saws^2)
+sum_yi2 <- sum(data$Total_Repair_Cost^2)
+sum_yimi <- sum(data$Total_Repair_Cost * data$Number_of_Saws)
+
+ybar <- sum_yi / sum_mi
+s2r <- (sum_yi2 - 2 * ybar * sum_yimi + ybar ^ 2 * sum_mi2) / (n - 1)
+var_ybar <- s2r / (sum_mi ^ 2 / n) * (1 - n / N)
 
 # (ii) Estimate the total amount spent by the 96 industries on band saw repairs and its variance:
 
 # Calculate the total amount spent by all 96 industries
-total_cost_all <- sum(data$Total_Repair_Cost)
-
-# Calculate the estimated total amount spent based on cluster sampling
-estimated_total_amount <- sample_mean * sum(data$Number_of_Saws)
+ybar_t <- sum_yi / n
+total_cost <- N * ybar_t
 
 # Calculate the variance of the estimated total amount spent
-
-# Calculate f (fraction of clusters sampled)
-f <- n / N
-
-# Calculate f_corr (finite population correction factor)
-f_corr <- (N - n) / (N - 1)
-
-# Assumed variance of the cluster means
-V_c <- 0.1 * sample_variance
-
-# Calculate the variance of the estimated total amount spent
-variance_estimate <- (N^2 / n) * ((1 - f) * (1 - f_corr) * sample_variance + f * (n - 1) * V_c)
+s2t <- 1 / (n - 1) * sum((data$Total_Repair_Cost - ybar_t)^2)
+var_total_cost <- N ^ 2 * (1 - n / N) * s2t / n
 
 # Print the results
-cat("Average Repair Cost per Saw (Sample Mean):", sample_mean, "\n")
-cat("Variance of Repair Costs per Saw (Sample Variance):", sample_variance, "\n")
-cat("Estimated Total Amount Spent by 96 Industries:", estimated_total_amount, "\n")
-cat("Variance of Estimated Total Amount:", variance_estimate, "\n")
+cat("Average Repair Cost per Saw:", ybar, "\n")
+cat("Variance of Repair Costs per Saw:", var_ybar, "\n")
+cat("Estimated Total Amount Spent by 96 Industries:", ybar_t, "\n")
+cat("Variance of Estimated Total Amount:", var_total_cost, "\n")
